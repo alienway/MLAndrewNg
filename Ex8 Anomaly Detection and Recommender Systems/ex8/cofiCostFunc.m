f617f7223e7f1edc7c318eee2265654e3d61e70e
Chris Lai <yawenlai25@gmail.com>
2021-06-05 17:10:47 +0800

    Anomaly Detection and Recommender Systems

--- Ex8 Anomaly Detection and Recommender Systems/ex8/cofiCostFunc.m
+++ Ex8 Anomaly Detection and Recommender Systems/ex8/cofiCostFunc.m
@@ -7,6 +7,7 @@ function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, ...
 %
 
 % Unfold the U and W matrices from params
+
 X = reshape(params(1:num_movies*num_features), num_movies, num_features);
 Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
@@ -40,9 +41,11 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+J = 1/2*sum(sum(((X*Theta'.*R - Y).^2))) + ...
+    lambda/2*sum(sum(Theta.^2)) + lambda/2*sum(sum(X.^2));
 
-
-
+X_grad = (X*Theta'.*R - Y)*Theta + lambda*X;
+Theta_grad = (X*Theta'.*R - Y)'*X + lambda*Theta;
 
 
 
